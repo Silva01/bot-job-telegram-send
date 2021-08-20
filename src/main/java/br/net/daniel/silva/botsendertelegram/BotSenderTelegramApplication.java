0d6e45b592024aa68eb3ad2d78b8974229f89458
daniel.cunha <daniel.cunha@redspark.io>
2021-08-20 17:17:10 -0300

    Initial Commit

---
+++ src/main/java/br/net/daniel/silva/botsendertelegram/BotSenderTelegramApplication.java
@@ -0,0 +1,13 @@
+package br.net.daniel.silva.botsendertelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotSenderTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotSenderTelegramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/net/daniel/silva/botsendertelegram/BotSenderTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.daniel.silva.botsendertelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotSenderTelegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
