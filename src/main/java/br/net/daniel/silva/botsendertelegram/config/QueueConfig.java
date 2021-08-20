890e5861543010afd3167e40a9de3b38b5e9116f
daniel.cunha <daniel.cunha@redspark.io>
2021-08-20 19:09:30 -0300

    Adicionado ajustes de configuração

--- src/main/java/br/net/daniel/silva/botsendertelegram/config/QueueConfig.java
+++ src/main/java/br/net/daniel/silva/botsendertelegram/config/QueueConfig.java
@@ -1,13 +1,22 @@
 package br.net.daniel.silva.botsendertelegram.config;
 
 import br.net.daniel.silva.botsendertelegram.receiver.Receiver;
+import br.net.daniel.silva.botsendertelegram.send.SendService;
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class QueueConfig {
+
+    private SendService sendService;
+
+    @Autowired
+    public QueueConfig(SendService sendService) {
+        this.sendService = sendService;
+    }
 
     @Bean
     public Queue send() {
@@ -17,6 +26,6 @@ public class QueueConfig {
     @Profile("receive")
     @Bean
     public Receiver receiver() {
-        return new Receiver();
+        return new Receiver(sendService);
     }
 }
--- src/main/java/br/net/daniel/silva/botsendertelegram/receiver/Receiver.java
+++ src/main/java/br/net/daniel/silva/botsendertelegram/receiver/Receiver.java
@@ -1,8 +1,12 @@
 package br.net.daniel.silva.botsendertelegram.receiver;
 
+import br.net.daniel.silva.botsendertelegram.send.SendService;
+import br.net.daniel.silva.botsendertelegram.util.ConvertUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -12,8 +16,19 @@ import java.nio.charset.StandardCharsets;
 @Component
 public class Receiver {
 
+    private SendService sendService;
+
+    @Autowired
+    public Receiver(SendService sendService) {
+        this.sendService = sendService;
+    }
+
     @RabbitHandler
     public void receive(byte[] mensagem) {
-        System.out.println(new String(mensagem, StandardCharsets.UTF_8));
+        try {
+            sendService.telegram(ConvertUtil.jsonToSendDTO(new String(mensagem, StandardCharsets.UTF_8)));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
