33ca36d3eb7f6446fb57e607d731677a899e29bb
silva01 <silva20102@gmail.com>
2021-08-21 21:24:43 -0300

    Ajustado para pegar valor de properties

--- src/main/java/br/net/daniel/silva/botsendertelegram/config/QueueConfig.java
+++ src/main/java/br/net/daniel/silva/botsendertelegram/config/QueueConfig.java
@@ -4,6 +4,7 @@ import br.net.daniel.silva.botsendertelegram.receiver.Receiver;
 import br.net.daniel.silva.botsendertelegram.send.SendService;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,6 +14,9 @@ public class QueueConfig {
 
     private SendService sendService;
 
+    @Value("${queue.url.send}")
+    private String queueNameUrl;
+
     @Autowired
     public QueueConfig(SendService sendService) {
         this.sendService = sendService;
@@ -20,7 +24,7 @@ public class QueueConfig {
 
     @Bean
     public Queue send() {
-        return new Queue("jarvis.send.telegram", true);
+        return new Queue(queueNameUrl, true);
     }
 
     @Profile("receive")
